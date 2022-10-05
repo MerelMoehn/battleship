acc209d2620a279bd1441a769277eefefd9884f5
MerelMoehn <merelmoehn@outlook.com>
2022-10-05 10:35:45 +0000

    Feat: Add introduction of new game function

--- run.py
+++ run.py
@@ -9,8 +9,27 @@ class Gameboard:
     """
 
     def __init__(self, name, btype):
-        self.num_ships = 4
-        self.board = [["." for x in range(5)] for y in range(5)]
         self.name = name
         self.btype = btype
+        self.num_ships = 4
+        self.board = [["." for x in range(5)] for y in range(5)]
+        self.guesses = []
+        self.shiploc = []
 
+
+def new_game():
+    """
+    Starts a new game. It start by resetting the scores, 
+    it displays the boards,
+    and will initialize the start of the game.
+    """
+
+    print("WELCOME! ARE YOU READY TO BATTLE?\n")
+    player_name = input("Enter your name please: \n")
+    print("-" * 30)
+    print(f"Hi, {player_name}\n")
+    print("The board grid is 5*5 and the number of ships is 4!\n")
+    print("Be aware, the top left corner is row: 0, col: 0\n")
+
+
+new_game()
