0cac7339a2b808c70279a1da03f9b6264077be49
MerelMoehn <merelmoehn@outlook.com>
2022-10-05 13:42:55 +0000

    Feat: Display locations of players ships

--- run.py
+++ run.py
@@ -10,9 +10,9 @@ class Gameboard:
     guesses and printing the board to the user.
     """
 
-    def __init__(self, name, btype):
+    def __init__(self, name, type):
         self.name = name
-        self.btype = btype
+        self.type = type
         self.num_ships = 4
         self.board = [["." for x in range(5)] for y in range(5)]
         self.guesses = []
@@ -22,15 +22,23 @@ class Gameboard:
     def print(self):
         for row in self.board:
             print(" ".join(row))
+    
+    def add_ship(self, x_cord, y_cord):
+        """
+        Adds ships to shiplocations list and shows
+        location of ship on playersboard.
+        """
 
+        if self.type == "Player":
+            self.shiploc.append((x_cord, y_cord))
+            self.board[x_cord][y_cord] = "@"
+            
 
-def populate_board(players_board):
-    new_ship_loc = []
-    for i in range(2):
-        cord = randint(0, 4)
-        new_ship_loc.append(cord)
-    players_board.shiploc.append(new_ship_loc)
-
+def populate_board(board):
+    x_cord = randint(0, 4)
+    y_cord = randint(0, 4)
+    board.add_ship(x_cord, y_cord)
+   
 
 def new_game():
     """
@@ -45,14 +53,13 @@ def new_game():
     print("The board grid is 5*5 and the number of ships is 4!\n")
     print("Be aware, the top left corner is row: 0, col: 0\n")
 
-    players_board = Gameboard(player_name, btype="Player")
-    computer_board = Gameboard("Computer", btype="Computer")
+    players_board = Gameboard(player_name, type="Player")
+    computer_board = Gameboard("Computer", type="Computer")
     
     for i in range(4):
         populate_board(players_board)
         populate_board(computer_board)
-    print(players_board.shiploc)
-    print(computer_board.shiploc)
+    print(players_board.print())
 
 
 print("WELCOME! ARE YOU READY TO BATTLE?\n")
